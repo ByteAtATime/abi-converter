3a9d80bed9da59fe710bd4882bfbc26dd3972b15
ByteAtATime <ByteAtATime@proton.me>
2024-05-27 15:40:25 -0700

    feat: copy code

--- packages/svelte/src/routes/[id]/_components/CodeDisplay.svelte
+++ packages/svelte/src/routes/[id]/_components/CodeDisplay.svelte
@@ -1,19 +1,36 @@
 <script lang="ts">
   import { codeToHtml } from "shiki";
   import { shikiThemes } from "$lib/shiki";
+  import { CheckCircle, DocumentDuplicate, Icon } from "svelte-hero-icons";
 
   const { code, lang } = $props();
 
   const highlightPromise = $derived(
     Promise.resolve(code).then(code => codeToHtml(code, { lang, themes: shikiThemes })),
   );
+
+  let copied = $state(false);
+
+  const copy = () => {
+    navigator.clipboard.writeText(code);
+    copied = true;
+
+    setTimeout(() => (copied = false), 1500);
+  };
 </script>
 
-<div class="flex w-full justify-center">
+<div class="relative flex w-full justify-center">
   {#await highlightPromise}
     <div class="loading loading-lg" />
   {:then highlighted}
     {@html highlighted}
+    <button class="btn btn-square btn-ghost absolute right-4 top-4" onclick={copy}>
+      {#if !copied}
+        <Icon src={DocumentDuplicate} class="h-6 w-6" />
+      {:else}
+        <Icon src={CheckCircle} class="h-6 w-6" />
+      {/if}
+    </button>
   {/await}
 </div>
 
